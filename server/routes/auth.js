714cc6dba8083fe18fa27b13e6609fa38c90455d
jessicawiradinata <jessicawiradinata@gmail.com>
2017-09-14 11:20:50 +1000

    Login validation with joi

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { login } from '../controllers/auth'
+import validation from '../../config/validations/auth'
+import validate from 'express-validation'
 
 const router = express.Router()
 
 router.route('/login')
 
-.post(login)
+.post(validate(validation.login), login)
 
 export default router
